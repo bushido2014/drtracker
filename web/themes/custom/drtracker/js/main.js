0dc2b792b34fc5e05923082beb93d69698e65377
Viorel Soltan <joomjoom2011@gmail.com>
2025-12-15 11:25:58 +0000

    create custom theme

---
+++ web/themes/custom/drtracker/js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Main JavaScript file for Drtracker theme
+ */
+
+(function() {
+  'use strict';
+
+  // Initialize theme
+  document.addEventListener('DOMContentLoaded', function() {
+    console.log('Drtracker theme initialized');
+  });
+
+})();
