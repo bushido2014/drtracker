a70461fb943072bca82b20856a22a69c32d3e066
Viorel Soltan <joomjoom2011@gmail.com>
2025-12-22 17:12:53 +0200

    Update main.js

--- web/themes/custom/drtracker/js/main.js
+++ web/themes/custom/drtracker/js/main.js
@@ -73,6 +73,31 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+ document.addEventListener('DOMContentLoaded', function() {
+  const buttons = document.querySelectorAll('[data-accordion-toggle]');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', function() {
+      const body = this.nextElementSibling;
+      const isActive = this.classList.contains('active');
+
+      // Close ALL buttons
+      buttons.forEach(btn => {
+        btn.classList.remove('active');
+        btn.nextElementSibling.classList.remove('show');
+      });
+
+      // Open clicked if not active
+      if (!isActive) {
+        this.classList.add('active');
+        body.classList.add('show');
+      }
+    });
+  });
+});
+
+
+      
 
       
     },
